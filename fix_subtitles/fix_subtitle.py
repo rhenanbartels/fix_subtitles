3b23676bffe2283ae514188878e929b0ddfb5bd9
LEP <lep@LEP.local>
2015-01-30 15:38:50 -0200

    Testing

--- fix_subtitles/fix_subtitle.py
+++ fix_subtitles/fix_subtitle.py
@@ -1,4 +1,4 @@
-#!usr/bin/env python
+#!/usr/bin/env python
 # -*- encoding:utf-8 -*-
 
 import argparse
@@ -9,20 +9,26 @@ pattern = "\d{2}:\d{2}:\d{2},\d{3}"
 
 
 def _open_file(file_name):
-    #text file extension.
+
+    #test file extension:.srt
     if file_name.endswith(".srt"):
         with open(file_name, "r") as file_object:
             file_content = file_object.read()
-            return file_content
+            file_content_list = file_content.split("\r\n")
+            return file_content_list
     else:
-        raise IOError:
-            print "File extension must be .srt"
+        raise IOError("File must have .srt extension!")
 
-def _get_file_list(file_content):
-    #break file in to lines
-    file_content = _open_file("test.txt").split("\r\n")
-    first_line = file_content
-    return first_line
+if __name__ == "__main__":
+    #Command line arguments: filename and offset.
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--file','-f', required=True, type=str, help=
+            'Subtitle filename to sync')
+    parser.add_argument('--offset','-o', required=True, type=int, help=
+            'Deslocation of subtitle in miliseconds (ms)')
+    args = parser.parse_args()
+    file_name = args.file
 
-
+    x =  _open_file(file_name)
+    print x
 
--- tests/test_fix_subtitle.py
+++ tests/test_fix_subtitle.py
@@ -1,14 +1,15 @@
+import argparse
 import unittest
+
 from fix_subtitle import _open_file
-from fix_subtitle import _get_file_list
 
 
 class TestFixSubtitle(unittest.TestCase):
-    def test_file_extension(self):
-        file_name = "test.txt"
-        self.assertRaises(IOError, _open_file, file_name)
-    def test_file_in_list(self):
-        file_list = "00:01:32,234 --> 00:01:55,134\n"
-        self.assertEqual(_get_file_list(_open_file("test.srt"))[0], file_list)
+    def test_open_file(self):
+        file_name = "requiem.srt"
+        with open(file_name, "r") as fobj:
+            file_first_line = fobj.readline()
+
+        self.assertEquals(_open_file(file_name)[0], file_first_line.strip())
 
 
